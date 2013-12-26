7fca8f111b6f069d83563abd45fc1eb06669695c
Marius Lundgård <marius.lundgard@gmail.com>
2013-12-27 00:17:04 +0100

    misc

--- src/Help/array.php
+++ src/Help/array.php
@@ -1,35 +1,33 @@
 <?php
 
-function array_insert($target, $index, $replacement)
-{
-    $result = [];
-
-    foreach ($target as $key => $value) {
-
-        if ($index === $key) {
-            $result[] = $replacement;
-        }
-
-        $result[] = $value;
-
-    }
-
-    return $result;
-
-  // $value = end($array);
-
-  // reset($array);
-
-  // return $value;
-}
-
-
 /**
- * Returns the last item of an array without modifying the array
+ * Inserts a value at a specific index 
+ * (an alternative to `array_splice`, but behaves a little differently).
  *
  * @param array &$array [description]
  *
  * @return [type]        [description]
+ */
+function array_insert_at_index($target, $index, $insertedValue)
+{
+    $result = [];
+
+    foreach ($target as $key => $value) {
+        if ($index === $key) {
+            $result[] = $insertedValue;
+        }
+        $result[] = $value;
+    }
+
+    return $result;
+}
+
+/**
+ * Returns the last item of an array without modifying the array
+ *
+ * @param array &$array The array to search
+ *
+ * @return mixed
  */
 function array_peek($array)
 {
@@ -43,8 +41,8 @@ function array_peek($array)
 /**
  * Recursively implode an array
  *
- * @param [type] $glue  [description]
- * @param [type] $array [description]
+ * @param string $glue  [description]
+ * @param array  $array [description]
  *
  * @return [type]        [description]
  */
@@ -63,10 +61,10 @@ function implode_recursive($glue, $array)
  * Sets a value in an array using a dot-separated path
  *
  * @param array  &$array [description]
- * @param [type] $path   [description]
- * @param [type] $value  [description]
+ * @param string $path   [description]
+ * @param mixed  $value  [description]
  *
- * @return [type]        [description]
+ * @return void
  */
 function array_dot_set(array &$array, $path, $value)
 {
@@ -112,18 +110,37 @@ function array_dot_get(array $subject, $path)
 
     while ((1 < count($keys)) && ($key = array_shift($keys))) {
 
-        if ((isset($subject[$key])) && (is_array($subject[$key]))) {
-            $subject = &$subject[$key];
+        if (is_array($subject)) {
+            if ((isset($subject[$key])) && (is_array($subject[$key]))) {
+                $subject = &$subject[$key];
+            }
+
+            // echo $key;
+            // exit;
+            if ((isset($subject[$key])) && (is_object($subject[$key]))) {
+                $subject = $subject[$key];
+                // echo "obj";
+                // exit;
+                // $subject = &$subject[$key];
+            }
         }
 
-        // echo $key;
-        // exit;
-        if ((isset($subject[$key])) && (is_object($subject[$key]))) {
-            $subject = $subject[$key];
-            // echo "obj";
+        elseif (is_object($subject)) {
+            if ((isset($subject->$key)) && (is_array($subject->$key))) {
+                $subject = &$subject->$key;
+            }
+
+            // echo $key;
             // exit;
-            // $subject = &$subject[$key];
+            if ((isset($subject->$key)) && (is_object($subject->$key))) {
+                $subject = $subject[$key];
+                // echo "obj";
+                // exit;
+                // $subject = &$subject[$key];
+            }
         }
+
+        
     }
 
     $key = array_shift($keys);
@@ -163,4 +180,42 @@ function array_dot_merge(array &$target, array $source)
 function is_assoc_array(array $array)
 {
     return array_keys($array) !== range(0, count($array) - 1);
+}
+
+/**
+ * Sorts an array of objects/arrays by a property.
+ *
+ * @param mixed  $array    The array to sort
+ * @param string $property The property to use for object sorting
+ *
+ * @return Boolean Returns TRUE on success or FALSE on failure.
+ */
+function sort_by_property(array &$array, $propertyKey)
+{
+    return usort($array, function($a, $b) use ($propertyKey)
+    {
+        // Check if object `a` is an object or array
+        if (is_object($a)) {
+            $aValue = $a->$propertyKey;
+        } elseif (is_array($a)) {
+            $aValue = $a[$propertyKey];
+        } else {
+            throw new \RuntimeException('Expected either an object or an array for sorting');
+        }
+
+        // Check if object `b` is an object or array
+        if (is_object($b)) {
+            $bValue = $b->$propertyKey;
+        } elseif (is_array($b)) {
+            $bValue = $b[$propertyKey];
+        } else {
+            throw new \RuntimeException('Expected either an object or an array for sorting');
+        }
+
+        if ($aValue === $bValue) {
+            return 0;
+        }
+
+        return ($aValue < $bValue) ? -1 : 1;
+    });
 }
