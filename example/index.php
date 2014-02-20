2840d73fc3668294d2562308b570b442d807e7c1
Marius Lundgård <marius.lundgard@gmail.com>
2014-02-20 19:49:19 +0100

    update

--- example/index.php
+++ example/index.php
@@ -1,16 +1,15 @@
 <?php
 
+namespace Util;
+
 require __DIR__.'/../vendor/autoload.php';
 
-$a = 'testing testing';
-$b = 'test test';
+$dict = new Dictionary([
+    'testing/this' => 'test1',
+    'testing/t' => 'test',
+    'testing/te' => 'test',
+], [
+    'delimiter' => '/',
+]);
 
-$diff = str_diff($a, $b);
-
-d($diff);
-
-$delta = str_delta_encode($a, $b);
-
-d($delta);
-
-// $text = str_delta_decode();
+d($dict['testing']);
